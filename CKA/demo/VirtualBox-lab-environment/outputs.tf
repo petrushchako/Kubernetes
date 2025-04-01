7e1c47721f10a0d418c7b71faa01a959b3ccc98f
Alex Petrushchak <petrushchako@gmail.com>
2025-04-01 20:44:08 +0100

    Remove terraform files

--- CKA/demo/VirtualBox-lab-environment/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "control_plane_vm" {
-  value = virtualbox_vm.control_plane.name
-}
-
-output "worker_nodes" {
-  value = [for w in virtualbox_vm.workers : w.name]
-}
--- CKA/demo/VirtualBox-lab-environment/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-vm_count = 3
-vm_cpus = 2
-vm_memory = 2048
-ubuntu_iso_path = "/path/to/ubuntu.iso"
